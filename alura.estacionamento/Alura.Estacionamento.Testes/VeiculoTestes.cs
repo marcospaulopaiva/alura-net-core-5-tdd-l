4a6f00c6d1eb173cee72b0e4cfc4e32ac5ec5fc6
Marcos Paulo <marcos.poo@gmail.com>
2024-02-15 20:11:05 -0300

    Aula 1 - Testes
    Nessa aula, aprendemos:
    - Podemos usar o Visual Studio Community para configurar um projeto de teste;
    - Podemos utilizar a CLI, a linha de comando, para criar e configurar nossa solução e projeto de teste;
    - Testes de unidade são testes que tem por objetivo testar as menores unidades de um código, as classes;
    - Usamos o padrão de escrita AAA (Arrange, Act, Assert) para permitir que a escrita do código de teste siga uma sequência lógica, que será útil para o entendimento e futuras manutenções;
    - Escrevemos códigos de testes com xUnit porque este é um dos mais utilizados frameworks de testes em .NET do mercado.

---
+++ alura.estacionamento/Alura.Estacionamento.Testes/VeiculoTestes.cs
@@ -0,0 +1,41 @@
+using Alura.Estacionamento.Alura.Estacionamento.Modelos;
+using Alura.Estacionamento.Modelos;
+using System;
+using Xunit;
+
+namespace Alura.Estacionamento.Testes
+{
+    public class VeiculoTestes
+    {
+        [Fact]
+        public void TestaVeiculoAcelerar()
+        {
+            //Arrange
+            var veiculo = new Veiculo();
+            //Act
+            veiculo.Acelerar(10);
+            //Assert
+            Assert.Equal(100, veiculo.VelocidadeAtual);
+        }
+
+        [Fact]
+        public void TestaVeiculoFrear()
+        {
+            //Arrange
+            var veiculo = new Veiculo();
+            //Act
+            veiculo.Frear(10);
+            //Assert
+            Assert.Equal(-150, veiculo.VelocidadeAtual);
+        }
+        [Fact]
+        public void TestaTipoVeiculo()
+        {
+            //Arrange
+            var veiculo = new Veiculo();
+            //Act
+            //Assert
+            Assert.Equal(TipoVeiculo.Automovel, veiculo.Tipo);
+        }
+    }
+}
